a0806f2ad725cc404f6c6dabe738bfd865073176
Esraa Saad <you@example.com>
2017-03-10 05:35:47 +0200

    First commit

--- src/project/RegExp.java
+++ src/project/RegExp.java
@@ -22,19 +22,19 @@ public class RegExp {
 		ArrayList<RegExp> regExps = new ArrayList<>();
 
 		
-		regExps.add(new RegExp("RIGHT_CURLY_B", "}"));
-		regExps.add(new RegExp("RIGHT_ROUND_B", ")"));
-		regExps.add(new RegExp("NOT", "!"));
-		regExps.add(new RegExp("MULTIPLY", "\\*"));
-		regExps.add(new RegExp("INT", "int"));
-		regExps.add(new RegExp("TRUE", "true"));
-		regExps.add(new RegExp("WHILE", "while"));
-		regExps.add(new RegExp("STATIC", "static"));
-		regExps.add(new RegExp("CHARACTER", "char"));
-		regExps.add(new RegExp("INTEGRAL_LITERAL", "\\d+"));
-		regExps.add(new RegExp("COMMENT1", "\\/\\/"));
-		regExps.add(new RegExp("COMMENT_L", "\\/\\*"));
-		regExps.add(new RegExp("CHAR", "\'\\w\'"));
+		regExps.add(new RegExp("RIGHT_CURLY_B", "\\b}\\b"));
+		regExps.add(new RegExp("RIGHT_ROUND_B", "\\b)\\b"));
+		regExps.add(new RegExp("NOT", "\\b!\\b"));
+		regExps.add(new RegExp("MULTIPLY", "\\b\\*\\b"));
+		regExps.add(new RegExp("INT", "\\bint\\b"));
+		regExps.add(new RegExp("TRUE", "\\btrue\\b"));
+		regExps.add(new RegExp("WHILE", "\\bwhile\\b"));
+		regExps.add(new RegExp("STATIC", "\\bstatic\\b"));
+		regExps.add(new RegExp("CHARACTER", "\\bchar\\b"));
+		regExps.add(new RegExp("INTEGRAL_LITERAL", "\\b\\d+\\b"));
+		regExps.add(new RegExp("COMMENT1", "\\b\\/\\/\\b"));
+		regExps.add(new RegExp("COMMENT_L", "\\b\\/\\*\\b"));
+		regExps.add(new RegExp("CHAR", "\\b\'.\'\\b"));
 
 		return regExps;
 
