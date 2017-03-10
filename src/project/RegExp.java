b1802947f7d457d25d8bdd5455c78e51fb6d6f48
Esraa Saad <you@example.com>
2017-03-10 19:10:27 +0200

    Edited RegExp

--- src/project/RegExp.java
+++ src/project/RegExp.java
@@ -6,11 +6,19 @@ public class RegExp {
 
 	public String type;
 	public String regExp;
-
+	public int startIndex;
+	public int endIndex;
 	public RegExp(String type, String regExp) {
 		super();
 		this.type = type;
 		this.regExp = regExp;
+	}
+
+	public RegExp(String type, String regExp,int startIndex) {
+		super();
+		this.type = type;
+		this.regExp = regExp;
+		this.startIndex = startIndex; 
 	}
 
 	public RegExp() {
@@ -20,21 +28,73 @@ public class RegExp {
 	public ArrayList<RegExp> fill() {
 
 		ArrayList<RegExp> regExps = new ArrayList<>();
+		
+		
+		regExps.add(new RegExp("COMMENT1", "\\b\\/\\/\\b"));
+		regExps.add(new RegExp("COMMENT2", "(\\/\\*).*(\\*\\/)"));
+		regExps.add(new RegExp("COMMENT_L", "\\b\\/\\*\\b"));
+		regExps.add(new RegExp("COMMENT_R", "\\b\\*\\/\\b"));
 
 		
-		regExps.add(new RegExp("RIGHT_CURLY_B", "\\b}\\b"));
-		regExps.add(new RegExp("RIGHT_ROUND_B", "\\b)\\b"));
-		regExps.add(new RegExp("NOT", "\\b!\\b"));
-		regExps.add(new RegExp("MULTIPLY", "\\b\\*\\b"));
-		regExps.add(new RegExp("INT", "\\bint\\b"));
-		regExps.add(new RegExp("TRUE", "\\btrue\\b"));
-		regExps.add(new RegExp("WHILE", "\\bwhile\\b"));
-		regExps.add(new RegExp("STATIC", "\\bstatic\\b"));
-		regExps.add(new RegExp("CHARACTER", "\\bchar\\b"));
-		regExps.add(new RegExp("INTEGRAL_LITERAL", "\\b\\d+\\b"));
-		regExps.add(new RegExp("COMMENT1", "\\b\\/\\/\\b"));
-		regExps.add(new RegExp("COMMENT_L", "\\b\\/\\*\\b"));
+		regExps.add(new RegExp("STRING_LITERAL", "\\b\"[^\"\\\\]*(\\\\.[^\"\\\\]*)*\"\\b"));
 		regExps.add(new RegExp("CHAR", "\\b\'.\'\\b"));
+
+		
+		regExps.add(new RegExp("FLOAT_LITERAL", "\\b[-]?\\d+\\.\\d+\\b"));
+		regExps.add(new RegExp("INTEGRAL_LITERAL", "\\b\\d+\\b"));
+		regExps.add(new RegExp("SYSTEM.OUT.PRINTLN", "\\bsystem\\.out\\.println\\b"));
+
+		
+		regExps.add(new RegExp("INT", "\\bint\\b"));
+		regExps.add(new RegExp("CHARACTER", "\\bchar\\b"));
+		regExps.add(new RegExp("STRING", "\\b(^String| String)\\b"));
+		regExps.add(new RegExp("BOOLEAN", "\\b(\\s|\\n)boolean\\s\\b"));
+		regExps.add(new RegExp("FLOAT", "\\bfloat\\b"));
+		
+		
+		regExps.add(new RegExp("CLASS", "\\b( class |^class)\\b"));
+		regExps.add(new RegExp("VOID", "\\b(\\s|\\n)void(\\s|\\n|(?=;))\\b"));
+		regExps.add(new RegExp("PUBLIC", "\\public\\b"));
+		regExps.add(new RegExp("STATIC", "\\bstatic\\b"));
+		regExps.add(new RegExp("NEW", "\\b(?<=(=|\\s))new\\s\\b"));
+		regExps.add(new RegExp("EXTENDS", "\\bextends\\b"));
+		regExps.add(new RegExp("TRUE", "\\btrue\\b"));
+		regExps.add(new RegExp("FALSE", "\\bfalse\\b"));
+		regExps.add(new RegExp("THIS", "\\bthis\\b"));
+		regExps.add(new RegExp("WHILE", "\\bwhile\\b"));
+		regExps.add(new RegExp("LENGTH", "\\b(?<=\\.)length(?=\\(\\))\\b"));
+		regExps.add(new RegExp("RETURN", "\\breturn\\b"));
+		regExps.add(new RegExp("IF", "\\bif\\b"));
+		regExps.add(new RegExp("ELSE", "\\b(\\s|\\n)else(\\s|\\n)\\b"));
+
+		
+		regExps.add(new RegExp("EOL", "(\\n)|($)"));
+		regExps.add(new RegExp("PLUS", "\\b\\+\\b"));
+		regExps.add(new RegExp("MINUS", "\\b-\\b"));
+		regExps.add(new RegExp("MULTIPLY", "\\b\\*\\b"));
+		regExps.add(new RegExp("EQUAL", "\\b=\\b"));
+
+		regExps.add(new RegExp("LEFT_SQUARE_B", "(?<=(\\s|\\w+))\\[(?=\\s|\\d+|\\])"));
+		regExps.add(new RegExp("RIGHT_SQUARE_B", "\\b\\]\\b"));
+		regExps.add(new RegExp("RIGHT_CURLY_B", "\\b}\\b"));
+		regExps.add(new RegExp("LEFT_CURLY_B", "\\b{\\b"));
+		regExps.add(new RegExp("LEFT_ROUND_B", "\\b(\\b"));
+		regExps.add(new RegExp("RIGHT_ROUND_B", "\\b)\\b"));
+
+		
+		regExps.add(new RegExp("COMMA", "\\b,\\b"));
+		regExps.add(new RegExp("SEMICOLON", "\\b;\\b"));
+		regExps.add(new RegExp("DOT", "\\b\\.\\b"));
+		regExps.add(new RegExp("NOT", "\\b!\\b"));
+		regExps.add(new RegExp("AND", "\\b&&\\b"));
+		regExps.add(new RegExp("LESSTHAN", "\\b<\\b"));
+		regExps.add(new RegExp("GREATERTHAN", "\\b>\\b"));
+		regExps.add(new RegExp("SQUOTE", "\\b‘\\b"));
+		regExps.add(new RegExp("DQUOTE", "\\b\"\\b"));
+
+		
+		regExps.add(new RegExp("MAIN","\bmain\b"));
+		regExps.add(new RegExp("ID", "\\b_?[a-zA-Z_]+\\w*\\b"));
 
 		return regExps;
 
--- src/project/main.java
+++ src/project/main.java
@@ -1,25 +1,34 @@
 package project;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 public class main {
 	public static ArrayList<RegExp> regExps = new ArrayList<>();
+	public static ArrayList<String> allTokens = new ArrayList<>();
+	public static ArrayList<Boolean> matched = new ArrayList<>();
+	public static HashMap<Integer, Token> Result = new HashMap<>();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		RegExp regExp = new RegExp();
 		regExps = regExp.fill();
-
-		String input = "int intval = 10*5";
-		tokenize(input);
-
-	}
-
-	public static void tokenize(String input) {
+		
+		
 		
 
 	}
 
+	
 }
